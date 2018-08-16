f48baee72899e63ad8524917da7fdd6f2c47db5c
pigletO <544388226@qq.com>
2018-08-16 18:05:59 +0800

    prictise3

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@EnableAutoConfiguration
+/*@RestController
+@EnableAutoConfiguration*/
+@Controller
 public class HelloController {
 
+
+    @ResponseBody //配合Controller使用
     @RequestMapping("/hello")
     public String hello(){
         return "myFirst SpringBoot Demo!!!!!";
